1eb6496433364c23580dff7f71ae445bc0bac76e
Manu-rca-git <Manuelramoncarrasco05@gmail.com>
2024-12-03 11:59:35 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+import "bootstrap";
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const card = document.getElementById("card");
+
+  // Definición de valores y palos
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = [
+    { name: "spade", symbol: "♠" },
+    { name: "club", symbol: "♣" },
+    { name: "heart", symbol: "♥" },
+    { name: "diamond", symbol: "♦" }
+  ];
+
+  // Selección aleatoria
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  // Configurar contenido y estilo
+  card.class = `card ${randomSuit}`;
+  card.innerHTML = `
+    <div class="top-left">${randomSuit.symbol}</div>
+    <div class="center">${randomValue}</div>
+    <div class="bottom-right">${randomSuit.symbol}</div>
+  `;
 };
